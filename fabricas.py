c1ff59bd8cb7cf199f41aeafed48be105d43a51a
Jonathan Campos <jc2222@gmail.com>
2021-04-19 18:42:19 -0500

    Se agrega el decorador

--- fabricas.py
+++ fabricas.py
@@ -1,17 +1,29 @@
 from productos import *
 from bridge import *
 
+def funcion_decorador(tipo):
+    tipos = ["1","2"]
+
+    def funcion_interior(clase):
+        class Extension(clase):
+            if tipo == "1":
+                def crear_otros(self):
+                    return AbstraccionRefinada(FabricaConcretaHumanos()) 
+            elif tipo == "2":
+                def crear_otros(self):
+                    return AbstraccionRefinada(FabricaConcretaOrcos()) 
+        return Extension
+    return funcion_interior
+
+@funcion_decorador(tipo = "1")
 class FabricaHumanos(Fabrica):
     def crear_arma(self):
         return ArmaHumanos()
 
     def crear_escudo(self):
         return EscudoHumanos()
-    
-    def crear_otros(self):
-        return AbstraccionRefinada(FabricaConcretaHumanos()) 
 
-
+@funcion_decorador(tipo = "2")
 class FabricaOrcos(Fabrica):
     def crear_arma(self):
         return ArmaOrcos()
@@ -19,5 +31,4 @@ class FabricaOrcos(Fabrica):
     def crear_escudo(self):
         return EscudoOrcos()
 
-    def crear_otros(self):
-        return AbstraccionRefinada(FabricaConcretaOrcos()) 
+
