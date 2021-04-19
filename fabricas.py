6502b995a57a0450556d41962a6b5b580daf4513
Jonathan Campos Lozano <58535230+Jcamposlozano@users.noreply.github.com>
2021-04-19 18:45:16 -0500

    se crea decorador

--- fabricas.py
+++ fabricas.py
@@ -1,6 +1,22 @@
 from productos import *
 from bridge import *
 
+
+def funcion_decorador(tipo):
+    tipos = ["1","2"]
+
+    def funcion_interior(clase):
+        class Extension(clase):
+            if tipo == "1":
+                def crear_otros(self):
+                    return AbstraccionRefinada(FabricaConcretaHumanos()) 
+            elif tipo == "2":
+                def crear_otros(self):
+                    return AbstraccionRefinada(FabricaConcretaOrcos()) 
+        return Extension
+    return funcion_interior
+
+@funcion_decorador(tipo = "1")
 class FabricaHumanos(Fabrica):
     def crear_arma(self):
         return ArmaHumanos()
@@ -11,7 +27,7 @@ class FabricaHumanos(Fabrica):
     def crear_otros(self):
         return AbstraccionRefinada(FabricaConcretaHumanos()) 
 
-
+@funcion_decorador(tipo = "2")
 class FabricaOrcos(Fabrica):
     def crear_arma(self):
         return ArmaOrcos()
